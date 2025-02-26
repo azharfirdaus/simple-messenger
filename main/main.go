822a1f578ea32f826e1ef097d8dce74093e6e6c7
Azhar Firdaus <azhar.firdaus9@gmail.com>
2025-02-27 00:19:52 +0700

    read config file (#1)
    * added .vscode
    
    * ignore .vscode folder
    
    * remove launch.json
    
    * read config file during startup program

--- main/main.go
+++ main/main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/azhar.firdaus/simple-messenger/routes"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	config, err := ReadConfig()
+	if err != nil {
+		return
+	}
+
 	// Register handlers
 	http.HandleFunc("/hello", routes.HelloHandler)
 
 	// Start the server
-	log.Println("Server started on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Printf("Server started on :%v", *config.Port)
+	port := ":" + *config.Port
+	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatalf("Could not start server: %v", err)
+		return
 	}
+}
+
+func ReadConfig() (*Config, error) {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalf("Error opening config file: %v", err)
+		return nil, err
+	}
+	defer file.Close()
+
+	var config Config
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalf("Error decoding JSON: %v", err)
+		return nil, err
+	}
+
+	return &config, nil
 }
